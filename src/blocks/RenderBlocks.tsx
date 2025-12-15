ce140647cab13ea17c3d290f89243be4efce7699
Nadeem AK <nadeemakhtar2399@gmail.com>
2025-12-15 17:06:32 +0500

    first sce

--- src/blocks/RenderBlocks.tsx
+++ src/blocks/RenderBlocks.tsx
@@ -40,7 +40,7 @@ export const RenderBlocks: React.FC<{
 
             if (Block) {
               return (
-                <div className="my-16" key={index}>
+                <div className="mb-16" key={index}>
                   <Block {...block} disableInnerContainer />
                 </div>
               )
